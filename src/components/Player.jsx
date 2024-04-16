0f9a62a8c651e2a769aae00b235745cc2de12488
Iryna Liashko <ira.liashko1@gmail.com>
2024-04-16 17:38:12 +0200

    Solution

---
+++ src/components/Player.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editiblePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editiblePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editiblePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
